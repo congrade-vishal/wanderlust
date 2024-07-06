1efea87f66c34eaef477d27ef6ca0dd139da0c59
Er. Vishal Kumar <sagarvishal877@gmail.com>
2024-07-06 16:56:54 +0530

    add new features & files

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js")
-const {listingSchema} = require("./schema.js")
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema, reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -36,8 +37,22 @@ app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
+
+// Listing backend validation function 
 const validateListing = (req, res, next)=>{
   let {error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else{
+    next();
+  }
+}
+
+
+// Review backend validation function 
+const validateReview = (req, res, next)=>{
+  let {error} = reviewSchema.validate(req.body);
   if(error){
     let errMsg = error.details.map((el)=>el.message).join(",");
     throw new ExpressError(400, errMsg);
@@ -91,6 +106,22 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
+}));
+
+// Review 
+// Post Route 
+app.post("/listings/:id/reviews", validateReview, wrapAsync( async(req, res)=>{
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  res.redirect(`/listings/${listing._id}`);
+
+  
 }));
 
 // app.get("/testListing", async (req, res) => {
